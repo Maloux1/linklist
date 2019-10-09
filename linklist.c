e73ae058e47942a1070762a9959ba01321587a6b
Rayon-Richter Malou <rayon.richter.malou@gmail.com>
2019-10-09 17:36:27 +0200

    initial commit

---
+++ linklist.c
@@ -0,0 +1,128 @@
+#include "linklist.h"
+
+list * createList(void * init(void *), void destroy(void *)){
+	list * head = malloc(sizeof(list));
+	if (head == NULL){
+		record(RECORD_LOG_ERROR, "LinkList", "malloc error", RECORD_CHECK_ERRNO, __FILE__, __LINE__);
+		return NULL;
+	}
+	head->first = NULL;
+	head->last = NULL;
+	head->size = 0;
+	head->init = init;
+	head->destroy = destroy;
+	return head;
+}
+
+void freeList(list * head){
+	elem * element = head->first, *next=NULL;
+	if (head->destroy){
+		while (element){
+			next = element->next;
+			head->destroy(element->data);
+			free(element);
+			element = next;
+		}
+	}
+	else {
+		while (element){
+			next = element->next;
+			free(element);
+			element = next;
+		}
+	}
+	free(head);
+	return;
+}
+
+int appendElement(list * head, void * dataOrArg){
+	elem * element = malloc(sizeof(elem));
+	if (element == NULL){
+		record(RECORD_LOG_ERROR, "LinkList", "malloc error", RECORD_CHECK_ERRNO, __FILE__, __LINE__);
+		return 1;
+	}
+	void * data;
+	if (head->init){
+		if ((data = head->init(dataOrArg)) == NULL){
+			record(RECORD_LOG_ERROR, "LinkList", "user defined function init error", RECORD_CHECK_ERRNO, __FILE__, __LINE__);
+			free(element);
+			return 1;
+		}
+	}
+	else {
+		data = dataOrArg;
+	}
+	element->next = NULL;
+	element->prev = NULL;
+	element->data = data;
+	if (head->first == NULL){
+		head->first = element;
+		head->last = element;
+		head->size = 1;
+	}
+	else {
+		head->last->next = element;
+		element->prev = head->last;
+		head->last = element;
+		head->size++;
+	}
+	return 0;
+}
+
+int prependElement(list * head, void * dataOrArg){
+	elem * element = malloc(sizeof(elem));
+	if (element == NULL){
+		record(RECORD_LOG_ERROR, "LinkList", "malloc error", RECORD_CHECK_ERRNO, __FILE__, __LINE__);
+		return 1;
+	}
+	void * data;
+	if (head->init){
+		if ((data = head->init(dataOrArg)) == NULL){
+			record(RECORD_LOG_ERROR, "LinkList", "user defined function init error", RECORD_CHECK_ERRNO, __FILE__, __LINE__);
+			free(element);
+			return 1;
+		}
+	}
+	else {
+		data = dataOrArg;
+	}
+	element->next = NULL;
+	element->prev = NULL;
+	element->data = data;
+	if (head->first == NULL){
+		head->first = element;
+		head->last = element;
+		head->size = 1;
+	}
+	else {
+		element->next = head->first;
+		element->next->prev = element;
+		head->first = element;
+		head->size++;
+	}
+	return 0;
+}
+
+uint32_t sizeofList(list * head){
+	return head->size;
+}
+
+void removeElement(list * head, elem * element){
+	if (head->destroy){
+		head->destroy(element->data);
+	}
+	if (element->prev == NULL){
+		head->first = element->next;
+	}
+	else {
+		element->prev->next = element->next;
+	}
+	if (element->next == NULL){
+		head->last = element->prev;
+	}
+	else {
+		element->next->prev = element->prev;
+	}
+	head->size--;
+	free(element);
+}
---
+++ linklist.h
@@ -0,0 +1,31 @@
+#ifndef LINKLIST_H
+#define LINKLIST_H
+
+#include <stdlib.h>
+#include <stdint.h>
+#include <record.h>
+
+typedef struct elem elem;
+struct elem{
+	void * data;
+	elem * prev;
+	elem * next;
+};
+
+typedef struct list list;
+struct list{
+	elem * first;
+	elem * last;
+	uint32_t size;
+	void * (*init)(void *);
+	void (*destroy)(void *);
+};
+
+list * createList(void * init(void *), void destroy(void *));
+void freeList(list *);
+int appendElement(list *, void *);
+int prependElement(list *, void *);
+uint32_t sizeofList(list *);
+void removeElement(list *, elem *);
+
+#endif /* LINKLIST_H */
